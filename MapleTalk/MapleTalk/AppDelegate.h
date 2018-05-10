51b0f987aa6f854193ffdc1802457376280d2403
Alice <18576759510@163.com>
2018-05-10 12:20:40 +0800

    Chore: init Project

---
+++ MapleTalk/MapleTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapleTalk
+//
+//  Created by Maple on 2018/5/10.
+//  Copyright © 2018 Maple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapleTalk/MapleTalk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapleTalk
+//
+//  Created by Maple on 2018/5/10.
+//  Copyright © 2018 Maple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
